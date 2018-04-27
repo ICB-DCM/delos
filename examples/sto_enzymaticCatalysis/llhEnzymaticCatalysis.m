a805d6b9befc38b170207fe70161536c817c7823
Paul Stapor <paul.stapor@helmholtz-muenchen.de>
2018-04-27 09:06:24 +0200

    bugfixed the EC xample

--- examples/sto_enzymaticCatalysis/llhEnzymaticCatalysis.m
+++ examples/sto_enzymaticCatalysis/llhEnzymaticCatalysis.m
@@ -9,7 +9,7 @@ function varargout = llhEnzymaticCatalysis(theta, miniBatch, amiData, amiOptions
         % Loop over minibatch
         for iBatch = miniBatch
             sol = simulate_model_EC_DELOS(amiData(iBatch).t, theta, amiData(iBatch).condition, amiData(iBatch), amiOptions);
-            J = J + sol.llh;
+            J = J - sol.llh;
         end
     elseif (nargout == 2)
         % Set options for gradient computation
@@ -19,8 +19,8 @@ function varargout = llhEnzymaticCatalysis(theta, miniBatch, amiData, amiOptions
         % Loop over minibatch
         for iBatch = miniBatch
             sol = simulate_model_EC_DELOS(amiData(iBatch).t, theta, amiData(iBatch).condition, amiData(iBatch), amiOptions);
-            J = J + sol.llh;
-            G = G + sol.sllh;
+            J = J - sol.llh;
+            G = G - sol.sllh;
         end
     elseif (nargout > 2)
         error('Call to logLikelihoodEC with too many output arguments')
